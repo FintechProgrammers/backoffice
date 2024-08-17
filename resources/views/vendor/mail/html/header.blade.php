f5266d4f9c268af2dba430a89528d0cde7e141d3
DEv Noel <ndamjoh@gmail.com>
2024-08-17 02:47:03 +0100

    email logo updated

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            @if (trim($slot) === 'Laravel')
-                <img src="{{ asset('assets/images/logo.png') }}" class="logo" alt="Laravel Logo">
-            @else
-                {{ $slot }}
-            @endif
+            <img src="{{ asset('assets/images/logo.webp') }}" class="logo" alt="Laravel Logo">
         </a>
     </td>
 </tr>
