82c04ca9803455f31c4c1f53febc2a53e18b4728
DEv Noel <ndamjoh@gmail.com>
2024-08-21 07:13:12 +0100

    debuggin create admins

--- app/Http/Controllers/Admin/AdministrativeUserController.php
+++ app/Http/Controllers/Admin/AdministrativeUserController.php
@@ -74,8 +74,9 @@ class AdministrativeUserController extends Controller
 
             return response()->json(['success' => true, 'message' => 'Admin created successfully']);
         } catch (\Exception $e) {
+            dd($e);
             DB::rollBack();
-            logger($e);
+            sendToLog($e);
             return response()->json(['success' => false, 'message' => serviceDownMessage()], 500);
         }
     }
