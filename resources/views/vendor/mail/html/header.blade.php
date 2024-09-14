30f9e8d945ffca37016b3719ced47b43c0261c4b
DEv Noel <ndamjoh@gmail.com>
2024-09-14 09:52:42 +0100

    updated

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('assets/images/logo.webp') }}" class="logo" alt="Laravel Logo">
+            <img src="{{ asset('assets/images/deltadigitallogo1.png') }}" class="logo" alt="Laravel Logo">
         </a>
     </td>
 </tr>
--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('app:release-commissions')->everyMinute();
+// Schedule::command('app:release-commissions')->everyMinute();
 
 Schedule::command('ranks:assign')->everyMinute();
 
